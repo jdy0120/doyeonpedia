90f69ecc7bbe7089b76a7bf0d23a3f7f3e248817
jdy0120 <jdy0120120@gmail.com>
2023-01-06 08:39:21 +0900

    include redux

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,20 @@
-import { useState } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/homePage/HomePage";
+import LoginPage from "./scenes/loginPage/LoginPage";
+import ProfilePage from "./scenes/profilePage/ProfilePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/types/type.ts
@@ -0,0 +1,7 @@
+export type User = {
+  friends: [];
+};
+
+export type Post = {
+  _id: string;
+};
